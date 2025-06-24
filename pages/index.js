8181ebfe7d9e73e0136f60f6acd8ca64b5da09f6
NFKBI <kristensen93@hotmail.com>
2025-06-24 14:31:59 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Velkommen til SkatteSkræddersyeren</h1>
+      <p>API'en er aktiv. Du kan nu modtage Stripe-webhooks på /api/webhook</p>
+    </main>
+  );
+}
